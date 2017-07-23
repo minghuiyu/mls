b0b4de3cab61791964b9eec2018fecb04ea9d209
minghuiyu <minghuiyu@gmail.com>
2017-07-23 12:40:26 -0700

    Update mls.php
    write to a file

--- mls.php
+++ mls.php
@@ -1,20 +1,33 @@
 <?php
 error_reporting ( E_ALL );
 ini_set ( 'display_errors', 1 );
+
 $array = explode ( "\n", $_POST ['textarea'] );
-$cities = array ();
+$line = array ();
 for($i = 0; $i < sizeof ( $array ); $i ++) {
 	preg_match ( '/[A-Z][a-z]+(\s[A-Z][a-z]+)?\s\$((\d{1,3},){1,2})(\d{3}\s)/', $array [$i], $matches );
-	$cities [] = $matches [0];
+	$line [] = $matches [0];
 }
-?>
-<table>
-<?php
-for($i = 0; $i < sizeof ( $cities ); $i ++) {
-	$cells = explode("$", $cities[$i]);
-	//print $cities [$i] . '<br>';
-	//print "<tr><td>".$cells[0]."</td><td>".$cells[1]."</td></tr>";
-	print "<tr><td>".$cells[1]."</td></tr>";
+
+$city = array();
+$price = array();
+for($i = 0; $i < sizeof ( $line ); $i ++) {
+	$value = explode("$", $line[$i]);
+    $city[] = $value[0]; 
+    $price[] = $value[1];
 }
+
+$date = date("m-Y",mktime(0, 0, 0, $_POST['month'], 1, $_POST['year']));
+
+array_unshift($city, ' ');
+array_unshift($price,$date);
+
+$filename = "singleDetachdHouse.txt";
+$filehandle = fopen($filename,"a");
+if (filesize($filename) == 0)
+	fputcsv($filehandle,$city);
+fputcsv($filehandle,$price);	
+fclose($filehandle);
+
+
 ?>
-</table>
